16decb188e02ef4f17026e06c12f96931845f672
Chris Mottram <C.Mottram@ljmu.ac.uk>
2001-02-01 11:16:57 +0000

    Initial revision

---
+++ ccd/test/posix_time.c
@@ -0,0 +1,76 @@
+/* posix_time.c -*- mode: Fundamental;-*-
+** $Header: /home/cjm/cvs/frodospec/ccd/test/posix_time.c,v 1.1 2001-02-01 11:16:25 cjm Exp $
+*/
+/**
+ * A little test program to prototype getting the current system time to as high a resolution as possible.
+ * This uses POSIX.4.
+ * Compile as follows:
+ * <pre>
+ * cc posix_time.c -o posix_time -lrt
+ * </pre>
+ * @author Chris Mottram
+ * @version $Revision: 1.1 $
+ */
+/**
+ * This hash define is needed before including source files give us POSIX.4/IEEE1003.1b-1993 prototypes
+ * for time.
+ */
+#define _POSIX_SOURCE 1
+/**
+ * This hash define is needed before including source files give us POSIX.4/IEEE1003.1b-1993 prototypes
+ * for time.
+ */
+#define _POSIX_C_SOURCE 199309L
+#include <stdio.h>
+#include <errno.h>
+#include <time.h>
+
+/**
+ * Revision Control System identifier.
+ */
+static char rcsid[] = "$Id: posix_time.c,v 1.1 2001-02-01 11:16:25 cjm Exp $";
+
+/* external routines */
+/**
+ * Main program. Gets and displays the resolution of the POSIX.4 Realtime clock.
+ */
+int main(int argc,char *argv[])
+{
+	struct timespec current_time[10];
+	struct timespec realtime_res;
+	struct timespec sleep_time;
+	char buff[256];
+	int retval,error_number,milliseconds,i;
+
+	retval = clock_getres(CLOCK_REALTIME,&realtime_res);
+	if(retval < 0)
+	{
+		error_number = errno;
+		fprintf(stderr,"posix_time:get resolution failed:%d\n",error_number);
+		return 1;
+	}
+	fprintf(stdout,"clock resolution:%d seconds:%d nanoseconds = %.3f milliseconds.\n",realtime_res.tv_sec,
+		realtime_res.tv_nsec,(((double)realtime_res.tv_nsec)/1000000.0));
+	for(i=0;i<10;i++)
+	{
+		clock_gettime(CLOCK_REALTIME,&(current_time[i]));
+	/* sleep for  */
+		sleep_time.tv_sec = 0;
+		/*sleep_time.tv_nsec = 1000000;*/ /* 1 millisecond */
+		/*sleep_time.tv_nsec = 10000000;*/ /* 100th second */
+		sleep_time.tv_nsec = 1; /* 1 nanosecond */
+		nanosleep(&sleep_time,NULL);
+	}
+	for(i=0;i<10;i++)
+	{
+		fprintf(stdout,"real time[%d]:%d seconds:%d nanoseconds.\n",i,current_time[i].tv_sec,
+			current_time[i].tv_nsec);
+		cftime(buff,"%Y-%m-%dT%H:%M:%S.",&current_time[i].tv_sec);
+		milliseconds = (((double)current_time[i].tv_nsec)/1000000.0);
+		fprintf(stdout,"time[%d]:%s%03d\n",i,buff,milliseconds);
+	}
+	return 0;
+}
+/*
+** $Log: not supported by cvs2svn $
+*/
---
+++ ccd/test/time_millis.c
@@ -0,0 +1,50 @@
+/* time_millis.c -*- mode: Fundamental;-*-
+** $Header: /home/cjm/cvs/frodospec/ccd/test/time_millis.c,v 1.1 2001-02-01 11:16:57 cjm Exp $
+*/
+/**
+ * A little test program to test returning the current system time in milliseconds.
+ * This uses POSIX.4.
+ * Compile as follows:
+ * <pre>
+ * cc -I${JNIINCDIR} -I${JNIMDINCDIR} time_millis.c -o time_millis -lrt
+ * </pre>
+ * @author Chris Mottram
+ * @version $Revision: 1.1 $
+ */
+/**
+ * This hash define is needed before including source files give us POSIX.4/IEEE1003.1b-1993 prototypes
+ * for time.
+ */
+#define _POSIX_SOURCE 1
+/**
+ * This hash define is needed before including source files give us POSIX.4/IEEE1003.1b-1993 prototypes
+ * for time.
+ */
+#define _POSIX_C_SOURCE 199309L
+#include <stdio.h>
+#include <limits.h>
+#include <errno.h>
+#include <time.h>
+#include <jni.h>
+
+/**
+ * Revision Control System identifier.
+ */
+static char rcsid[] = "$Id: time_millis.c,v 1.1 2001-02-01 11:16:57 cjm Exp $";
+
+/* external routines */
+/**
+ * Main program. Gets and displays the resolution of the POSIX.4 Realtime clock.
+ */
+int main(int argc,char *argv[])
+{
+	struct timespec current_time;
+	jlong retval;
+
+	clock_gettime(CLOCK_REALTIME,&(current_time));
+	retval = ((jlong)current_time.tv_sec)*((jlong)1000L);
+	retval += ((jlong)current_time.tv_nsec)/((jlong)1000000L);
+	fprintf(stdout,"jlong milliseconds:%lld\n",retval);
+
+	return 0;
+}
