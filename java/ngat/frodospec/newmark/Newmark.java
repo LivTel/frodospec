83ef33167b80247a8c844a7e9c5ee7d9f6d7b432
Chris Mottram <C.Mottram@ljmu.ac.uk>
2011-01-05 14:13:02 +0000

    Changed API so clazz and source are passed into calls, to improve logging.

--- java/ngat/frodospec/newmark/Newmark.java
+++ java/ngat/frodospec/newmark/Newmark.java
@@ -1,5 +1,5 @@
 // Newmark.java
-// $Header: /home/cjm/cvs/frodospec/java/ngat/frodospec/newmark/Newmark.java,v 1.2 2009-02-05 11:40:07 cjm Exp $
+// $Header: /home/cjm/cvs/frodospec/java/ngat/frodospec/newmark/Newmark.java,v 1.3 2011-01-05 14:13:02 cjm Exp $
 package ngat.frodospec.newmark;
 
 import java.lang.*;
@@ -12,14 +12,14 @@ import ngat.serial.arcomess.*;
  * allows communication with the motion controller's serial interface either directly or via
  * a Arcom ESS (Ethernet Serial Server).
  * @author Chris Mottram
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class Newmark
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class
 	 */
-	public final static String RCSID = new String("$Id: Newmark.java,v 1.2 2009-02-05 11:40:07 cjm Exp $");
+	public final static String RCSID = new String("$Id: Newmark.java,v 1.3 2011-01-05 14:13:02 cjm Exp $");
 //internal C layer initialisation
 	/**
 	 * Native method that allows the JNI layer to store a reference to this Class's logger.
@@ -48,24 +48,42 @@ public class Newmark
 // newmark_command.h
 	/**
 	 * Native wrapper to libfrodospec_newmark routine that homes the mechanism.
+	 * @param clazz A string representing the class that generated log messages originiting
+	 *        from this method call. 
+	 * @param source A string representing the source that generated log messages originiting
+	 *        from this method call. 
 	 */
-	private native void Newmark_Command_Home() throws NewmarkNativeException;
+	private native void Newmark_Command_Home(String clazz,String source) throws NewmarkNativeException;
 	/**
 	 * Native wrapper to libfrodospec_newmark routine that moves the mechanism.
 	 */
-	private native void Newmark_Command_Move(double position) throws NewmarkNativeException;
+	private native void Newmark_Command_Move(String clazz,String source,double position) throws 
+		NewmarkNativeException;
 	/**
 	 * Native wrapper to libfrodospec_newmark routine that aborts movement.
+	 * @param clazz A string representing the class that generated log messages originiting
+	 *        from this method call. 
+	 * @param source A string representing the source that generated log messages originiting
+	 *        from this method call. 
 	 */
-	private native void Newmark_Command_Abort_Move() throws NewmarkNativeException;
+	private native void Newmark_Command_Abort_Move(String clazz,String source) throws NewmarkNativeException;
 	/**
 	 * Native wrapper to libfrodospec_newmark routine that gets the current mechanism position.
+	 * @param clazz A string representing the class that generated log messages originiting
+	 *        from this method call. 
+	 * @param source A string representing the source that generated log messages originiting
+	 *        from this method call. 
 	 */
-	private native double Newmark_Command_Position_Get() throws NewmarkNativeException;
+	private native double Newmark_Command_Position_Get(String clazz,String source) throws NewmarkNativeException;
 	/**
 	 * Native wrapper to libfrodospec_newmark routine that sets the 'in-position' tolerance.
+	 * @param clazz A string representing the class that generated log messages originiting
+	 *        from this method call. 
+	 * @param source A string representing the source that generated log messages originiting
+	 *        from this method call. 
 	 */
-	private native void Newmark_Command_Position_Tolerance_Set(double mm) throws NewmarkNativeException;
+	private native void Newmark_Command_Position_Tolerance_Set(String clazz,String source,double mm) throws 
+		NewmarkNativeException;
 
 // per instance variables
 	/**
@@ -136,7 +154,21 @@ public class Newmark
 	 */
 	public void home() throws NewmarkNativeException
 	{
-		Newmark_Command_Home();
+		Newmark_Command_Home(this.getClass().getName(),null);
+	}
+
+	/**
+	 * Method to home the mechanism.
+	 * @param clazz A string representing the class to put in the log record for all logs generated
+	 *              by this call.
+	 * @param source A string representing the source to put in the log record for all logs generated
+	 *              by this call.
+	 * @exception NewmarkNativeException This method throws a NewmarkNativeException if it failed.
+	 * @see #Newmark_Command_Home
+	 */
+	public void home(String clazz,String source) throws NewmarkNativeException
+	{
+		Newmark_Command_Home(clazz,source);
 	}
 
 	/**
@@ -147,7 +179,22 @@ public class Newmark
 	 */
 	public void move(double position) throws NewmarkNativeException
 	{
-		Newmark_Command_Move(position);
+		Newmark_Command_Move(this.getClass().getName(),null,position);
+	}
+
+	/**
+	 * Method to move the mechanism.
+	 * @param clazz A string representing the class to put in the log record for all logs generated
+	 *              by this call.
+	 * @param source A string representing the source to put in the log record for all logs generated
+	 *              by this call.
+	 * @param position The new position of the mechanism in mm. Valid range around -25 to 175 for our mechanism.
+	 * @exception NewmarkNativeException This method throws a NewmarkNativeException if it failed.
+	 * @see #Newmark_Command_Move
+	 */
+	public void move(String clazz,String source,double position) throws NewmarkNativeException
+	{
+		Newmark_Command_Move(clazz,source,position);
 	}
 
 	/**
@@ -157,7 +204,21 @@ public class Newmark
 	 */
 	public void abort() throws NewmarkNativeException
 	{
-		Newmark_Command_Abort_Move();
+		Newmark_Command_Abort_Move(this.getClass().getName(),null);
+	}
+
+	/**
+	 * Method to abort any ongoing movement.
+	 * @param clazz A string representing the class to put in the log record for all logs generated
+	 *              by this call.
+	 * @param source A string representing the source to put in the log record for all logs generated
+	 *              by this call.
+	 * @exception NewmarkNativeException This method throws a NewmarkNativeException if it failed.
+	 * @see #Newmark_Command_Abort_Move
+	 */
+	public void abort(String clazz,String source) throws NewmarkNativeException
+	{
+		Newmark_Command_Abort_Move(clazz,source);
 	}
 
 	/**
@@ -168,7 +229,22 @@ public class Newmark
 	 */
 	public double getPosition() throws NewmarkNativeException
 	{
-		return Newmark_Command_Position_Get();
+		return Newmark_Command_Position_Get(this.getClass().getName(),null);
+	}
+
+	/**
+	 * Method to get the current position of the mechanism.
+	 * @param clazz A string representing the class to put in the log record for all logs generated
+	 *              by this call.
+	 * @param source A string representing the source to put in the log record for all logs generated
+	 *              by this call.
+	 * @return The current position of the mechanism in mm. Valid range around -25 to 175 for our mechanism.
+	 * @exception NewmarkNativeException This method throws a NewmarkNativeException if it failed.
+	 * @see #Newmark_Command_Position_Get
+	 */
+	public double getPosition(String clazz,String source) throws NewmarkNativeException
+	{
+		return Newmark_Command_Position_Get(clazz,source);
 	}
 
 	/**
@@ -180,12 +256,31 @@ public class Newmark
 	 */
 	public void setPositionTolerance(double mm) throws NewmarkNativeException
 	{
-		Newmark_Command_Position_Tolerance_Set(mm);
+		Newmark_Command_Position_Tolerance_Set(this.getClass().getName(),null,mm);
+	}
+
+	/**
+	 * Method to set how close the reported stage position has to be to the requested stage position
+	 * for the stage to be 'in position'.
+	 * @param clazz A string representing the class to put in the log record for all logs generated
+	 *              by this call.
+	 * @param source A string representing the source to put in the log record for all logs generated
+	 *              by this call.
+	 * @param mm The tolerance in mm. Should be between 0.0..1.0 mm.
+	 * @exception NewmarkNativeException This method throws a NewmarkNativeException if it failed.
+	 * @see #Newmark_Command_Position_Tolerance_Set
+	 */
+	public void setPositionTolerance(String clazz,String source,double mm) throws NewmarkNativeException
+	{
+		Newmark_Command_Position_Tolerance_Set(clazz,source,mm);
 	}
 }
  
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2009/02/05 11:40:07  cjm
+// Swapped Bitwise for Absolute logging levels.
+//
 // Revision 1.1  2008/11/20 11:34:35  cjm
 // Initial revision
 //
